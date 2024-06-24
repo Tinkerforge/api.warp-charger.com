abd67c727f9f8fd33b4a2d481f64918716b0997f
Olaf Lüke <olaf@tinkerforge.com>
2024-06-24 23:10:49 +0200

    drastically simplify day ahead prices api (no historic values)

---
+++ main.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from services import day_ahead_prices
+from flask import Flask, Blueprint
+import logging
+import os
+import threading
+import time
+
+DEFAULT_PORT = 5000
+
+main_api = Blueprint('main_api', __name__)
+@main_api.route("/")
+def home():
+    return "<html>This is api.warp-charger.com.</br>This API is used by WARP Chargers to obtain relevant public information like day ahead prices.</html>"
+
+def backend_tasks():
+    global running
+    day_ahead_prices.update()
+    running = True
+
+    while running:
+        time.sleep(5*60)
+        try:
+            day_ahead_prices.update()
+        except:
+            logging.error("Exception during day ahead price update", exc_info=True)
+
+# Flask init
+app = Flask(__name__)
+app.register_blueprint(main_api)
+app.register_blueprint(day_ahead_prices.day_ahead_prices_api)
+app.config["JSON_SORT_KEYS"] = False
+
+port = int(os.environ.get('PORT', DEFAULT_PORT))
+running = False
+
+backend_thread = threading.Thread(target=backend_tasks)
+logging.basicConfig(level=logging.DEBUG, format="[%(levelname)-8s%(filename)s:%(lineno)s] %(message)s")
+backend_thread.start()
+
+# Wait for first data update
+while not running:
+    time.sleep(1)
+
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=False, host="0.0.0.0", port=port)
+
+    running = False
+    backend_thread.join()
--- services/day_ahead_prices.py
+++ services/day_ahead_prices.py
@@ -1,147 +1,156 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import logging
 import os
-import sqlite3
 import entsoe
-from entsoe import EntsoePandasClient
 import pandas as pd
-from flask import Flask, jsonify, g
+from flask import Blueprint
 from datetime import datetime, timedelta
-from cachetools import cached, LRUCache
-from threading import Lock
+import time
+import json
+from collections import OrderedDict
 
-DEFAULT_PORT = 5000
+day_ahead_prices_api = Blueprint('day_ahead_prices_api', __name__)
+
 FILE_DIR = os.path.dirname(os.path.realpath(__file__))
 PROJET_DIR = os.path.abspath(os.path.join(FILE_DIR, '..'))
 WARP_DB_FILE = os.path.join(PROJET_DIR, "warp.db")
 ENTSOE_KEY = open(os.path.join(PROJET_DIR, "entsoe.key")).read().strip()
 
 logger = logging.getLogger(__name__)
-day_ahead_prices_lock = Lock()
+entsoe_client = entsoe.EntsoePandasClient(api_key=ENTSOE_KEY)
 
-# Flask init
-app = Flask(__name__)
-port = int(os.environ.get('PORT', DEFAULT_PORT))
+DAY_AHEAD_PRICE_NOT_FOUND   = '{"error":"Data not found"}', 404
+DAY_AHEAD_PRICE_DE_LU_15MIN = 0
+DAY_AHEAD_PRICE_DE_LU_60MIN = 1
+DAY_AHEAD_PRICE_AT_15MIN    = 2
+DAY_AHEAD_PRICE_AT_60MIN    = 3
 
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(WARP_DB_FILE)
-    return db
+def daps():
+    yield DAY_AHEAD_PRICE_DE_LU_15MIN, 'DE_LU', '15min'
+    yield DAY_AHEAD_PRICE_DE_LU_60MIN, 'DE_LU', '60min'
+    yield DAY_AHEAD_PRICE_AT_15MIN,    'AT',    '15min'
+    yield DAY_AHEAD_PRICE_AT_60MIN,    'AT',    '60min'
 
-# TODO: Only allow one update per x minutes per date
-def update_day_ahead_prices(country, resolution, date):
+dap_list = [DAY_AHEAD_PRICE_NOT_FOUND]*4
+
+def update_day_ahead_prices(client, country_code, resolution):
+    logging.debug("Getting day ahead prices for country {0} with resolution {1}".format(country_code, resolution))
     try:
-        # Currently only DE_LU is supported
-        if country in ('de', 'lu'):
-            country_code = 'DE_LU'
-        elif country == 'at':
-            country_code = 'AT'
-        else:
-            logging.warning("Country not supported: {0}".format(country))
-            return None
+        start = pd.Timestamp.today(tz='Europe/Berlin').replace(hour=0, minute=0, second=0, microsecond=0)
+        end = start + timedelta(days=7)
 
-        start  = pd.Timestamp(date, tz='Europe/Berlin')
-        end    = start + timedelta(minutes=60*24-1)
-        client = EntsoePandasClient(api_key=ENTSOE_KEY)
         ts     = client.query_day_ahead_prices(country_code, start=start, end=end, resolution=resolution)
         data   = ts.to_list()
         # Check if data has valid number of entries
-        if resolution == '15min' and len(data) != 4*24:
+        if resolution == '15min' and len(data) < 23*4:
             logging.warning("Invalid number of entries for 15min: {0}".format(len(data)))
             return None
-        if resolution == '60min' and len(data) != 24:
+        if resolution == '60min' and len(data) < 23:
             logging.warning("Invalid number of entries for 60min: {0}".format(len(data)))
             return None
 
         # convert from cent in float to centicent in int
-        data_str = ','.join([str(int(i*100)) for i in data])
-        logging.debug("Discovered new day ahead prices for {0}: {1}".format(date, data_str))
+        first_date_ts = int(start.timestamp())
+        prices = [int(i*100) for i in data]
+        # If we got day ahead prices ask again tomorrow, else ask again today
+        if (resolution == '15min' and len(data) < 25*4) or (resolution == '60min' and len(data) < 25):
+            next_date = start.replace(hour=14, minute=0, second=0, microsecond=0)
+        else:
+            next_date = (start + timedelta(days=1)).replace(hour=14, minute=0, second=0, microsecond=0)
+        next_date_ts = int(next_date.timestamp())
+        logging.debug("Discovered new day ahead prices for {0}/{1}: {2}, next: {3}/{4}".format(str(start), str(first_date_ts), str(prices), str(next_date), str(next_date_ts)))
 
-        con = get_db()
-        cur = con.cursor()
-        logging.debug("Inserting new day ahead prices for {0} into sqlite".format(date))
-        cur.execute("INSERT INTO day_ahead_prices (country, resolution, date, data) VALUES (?, ?, ?, ?)", (country, resolution, date, data_str))
-        con.commit()
     except entsoe.exceptions.NoMatchingDataError:
-        logging.debug('No matching data in entso-e database found for date {0}'.format(date))
+        logging.debug('No matching data in entso-e database found for date {0}'.format(str(start)))
         return None
     except:
         logging.error("Exception during entso-e query", exc_info=True)
         return None
 
-    return data_str
+    # Generate odered json without spaces
+    od = OrderedDict()
+    od['first_date'] = first_date_ts
+    od['prices']     = prices
+    od['next_date']  = next_date_ts
+    return json.dumps(od, separators=(',', ':')), 200
 
-@cached(cache=LRUCache(maxsize=1000))
-def get_day_ahead_prices_cached(country, resolution, date):
-    cur = get_db().cursor()
-    res = cur.execute("SELECT data FROM day_ahead_prices WHERE country = ? AND resolution = ? AND date = ?", (country, resolution, date))
-    data = res.fetchone()
-    if data != None:
-        data = data[0]
-        logging.debug("data sqlite: {0}".format(data))
-    else:
-        logging.debug("No data found in sqlite for date {0}".format(date))
-        data = update_day_ahead_prices(country, resolution, date)
-        logging.debug("data entso-e: {0}".format(data))
-        if data == None:
-            # Throw exception if no data was found
-            # This way the None-value will not be saved in the cache
-            raise Exception("No data")
+def update_day_ahead_prices_with_retry(client, country_code, resolution, retries=5):
+    for retry in range(retries):
+        value = update_day_ahead_prices(client, country_code, resolution)
+        if value != None:
+            return value
+        # Wait one minute more with every retry 
+        time.sleep(60*retry)
+    return DAY_AHEAD_PRICE_NOT_FOUND
 
-    return list(map(int, data.split(',')))
-
-def get_day_ahead_prices(country, resolution, date):
+def is_update_necessary(dap, min_price_list_length):
     try:
-        prices = get_day_ahead_prices_cached(country, resolution, date)
+        if dap == None:
+            logging.debug("Update because dap == None")
+            return True
+
+        if type(dap) != tuple:
+            logging.debug("Update because dap != tuple ({0})".format(str(dap)))
+            return True
+
+        if dap == DAY_AHEAD_PRICE_NOT_FOUND:
+            logging.debug("Update because dap == DAY_AHEAD_PRICE_NOT_FOUND")
+            return True
+
+        dap0_dict = json.loads(dap[0])
+        dap0_next_date = dap0_dict.get('next_date')
+        dap0_prices = dap0_dict.get('prices')
+        if (dap0_next_date == None) or (dap0_prices == None):
+            logging.debug("Update because JSON malformed ({0})".format(str(dap)))
+            return True
+
+        d1 = int(dap0_next_date)
+        d2 = int(datetime.now().timestamp())
+        if d1 < d2:
+            logging.debug("Update because {0} < {1}".format(d1, d2))
+            return True
+
+        # Check if price list only contains one day of prices
+        # In that case we did not get the day ahead prices the last time we updated the data
+        d1 = len(dap0_prices)
+        d2 = min_price_list_length
+        if d1 < d2:
+            logging.debug("Update because price list too small {0} < {1}".format(d1, d2))
+
+        logging.debug("No update necessary")
+        return False
     except:
-        return None
+        logging.error("Exception during entso-e query", exc_info=True)
+        return True
 
-    return prices
+def update():
+    for dap, country_code, resolution in daps():
+        logging.debug("Check update for {0} {1}".format(country_code, resolution))
+        if is_update_necessary(dap_list[dap], 25 if resolution == '60min' else 25*4):
+            dap_list[dap] = update_day_ahead_prices_with_retry(entsoe_client, country_code, resolution)
 
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
-
-@app.route("/")
-def home():
-    return "<html>This is api.warp-charger.com.</br>This API can be used by WARP Charger to obtain relevant public information like day ahead prices."
-
-# TODO: Rate limite per IP
-@app.route('/v1/day_ahead_prices/<country>/<resolution>/<date>', methods=['GET'])
-def day_ahead_prices(country, resolution, date):
+# TODO: Rate limit per IP
+@day_ahead_prices_api.route('/v1/day_ahead_prices/<country>/<resolution>', methods=['GET'])
+def day_ahead_prices(country, resolution):
     country    = country.lower()
     resolution = resolution.lower()
 
     if not country in ('de', 'lu', 'at'):
         logging.info("Country not supported: {0}".format(country))
-        return jsonify({"error": "Country not supported"}), 400
+        return '{"error":"Country not supported"}', 400
     if not resolution in('15min', '60min'):
         logging.info("Resolution not supported: {0}".format(resolution))
-        return jsonify({"error": "Resolution not supported"}), 400
-    try:
-        dt = datetime.strptime(date, "%Y-%m-%d")
-    except ValueError:
-        logging.info("Date format not supported: {0}".format(date))
-        return jsonify({"error": "Date format not supported"}), 400
+        return '{"error":"Resolution not supported"}', 400
 
-    # Only allow one conneciton at a time to get the day ahead prices.
-    # This way we can be sure that there can't be any race conditions with
-    # the cache, the sqlite database and the entso-e API.
-    with day_ahead_prices_lock:
-        prices = get_day_ahead_prices(country, resolution, date)
+    if country in ('de', 'lu') and resolution == '15min':
+        return dap_list[DAY_AHEAD_PRICE_DE_LU_15MIN]
+    elif country in ('de', 'lu') and resolution == '60min':
+        return dap_list[DAY_AHEAD_PRICE_DE_LU_60MIN]
+    elif country == 'at' and resolution == '15min':
+        return dap_list[DAY_AHEAD_PRICE_AT_15MIN]
+    elif country == 'at' and resolution == '60min':
+        return dap_list[DAY_AHEAD_PRICE_AT_60MIN]
 
-    if prices == None:
-        return jsonify({"error": "Data not found (too far into the future?)"}), 404
-
-    return jsonify({"date": date, "prices": prices}), 200
-
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
-    app.run(debug=True, host="0.0.0.0", port=port)
+    logging.error("Reached unreachable code")
+    return '{"error":"Unknown error"}', 404
