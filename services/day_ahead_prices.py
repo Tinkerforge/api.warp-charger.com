92a408c0cf3ae3749d7fe5392000b4026db1b5c2
Olaf Lüke <olaf@tinkerforge.com>
2024-10-06 20:41:41 +0200

    Remove entsoe-py dependency
    Entso-E changed the format of the XML and entsoe-py is not compatible
    anymore for now. We may revert this at some point when entsoe-py is
    updated.

--- services/day_ahead_prices.py
+++ services/day_ahead_prices.py
@@ -2,13 +2,15 @@
 
 import logging
 import os
-import entsoe
+from urllib.request import urlopen
+from xml.etree import ElementTree
 import pandas as pd
 from flask import Blueprint
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 import time
 import json
 from collections import OrderedDict
+import re
 
 day_ahead_prices_api = Blueprint('day_ahead_prices_api', __name__)
 
@@ -18,7 +20,6 @@ WARP_DB_FILE = os.path.join(PROJET_DIR, "warp.db")
 ENTSOE_KEY = open(os.path.join(PROJET_DIR, "entsoe.key")).read().strip()
 
 logger = logging.getLogger(__name__)
-entsoe_client = entsoe.EntsoePandasClient(api_key=ENTSOE_KEY)
 
 DAY_AHEAD_PRICE_NOT_FOUND   = '{"error":"Data not found"}', 404
 DAY_AHEAD_PRICE_DE_LU_15MIN = 0
@@ -27,20 +28,65 @@ DAY_AHEAD_PRICE_AT_15MIN    = 2
 DAY_AHEAD_PRICE_AT_60MIN    = 3
 
 def daps():
-    yield DAY_AHEAD_PRICE_DE_LU_15MIN, 'DE_LU', '15min'
-    yield DAY_AHEAD_PRICE_DE_LU_60MIN, 'DE_LU', '60min'
-    yield DAY_AHEAD_PRICE_AT_15MIN,    'AT',    '15min'
-    yield DAY_AHEAD_PRICE_AT_60MIN,    'AT',    '60min'
+    yield DAY_AHEAD_PRICE_DE_LU_15MIN, '10Y1001A1001A82H', 'PT15M' # DE_LU
+    yield DAY_AHEAD_PRICE_DE_LU_60MIN, '10Y1001A1001A82H', 'PT60M' # DE_LU
+    yield DAY_AHEAD_PRICE_AT_15MIN,    '10YAT-APG------L', 'PT15M' # AT
+    yield DAY_AHEAD_PRICE_AT_60MIN,    '10YAT-APG------L', 'PT60M' # AT
 
 dap_list = [DAY_AHEAD_PRICE_NOT_FOUND]*4
 
-def update_day_ahead_prices(client, country_code, resolution):
+def parse_timeseries(xml_text, resolution, value_key='price.amount'):
+    resolution_map = {
+        'PT60M': pd.Timedelta(60, 'min'),
+        'P1Y'  : pd.Timedelta(365,'day'),
+        'PT15M': pd.Timedelta(15, 'min'),
+        'PT30M': pd.Timedelta(30, 'min'),
+        'P1D'  : pd.Timedelta(1,  'day'),
+        'P7D'  : pd.Timedelta(7,  'day'),
+        'P1M'  : pd.Timedelta(30, 'day'),
+    }
+    pdres       = resolution_map[resolution]
+    time_stamps = []
+    values      = []
+    xml_text    = re.sub(' xmlns="[^"]+"', '', xml_text, count=1) #Remove namespace
+    root        = ElementTree.fromstring(xml_text)
+    for time_serie in root.findall('TimeSeries'):
+        #curve_type = time_serie.find('curveType').text
+        for period in time_serie.findall('Period'):
+            start_time = pd.Timestamp(period.find('timeInterval').find('start').text)
+            if period.find('resolution').text == resolution:
+                for point in period.findall('Point'):
+                    position = int(point.find('position').text)-1
+                    value    = float(point.find(value_key).text)
+                    time_stamps.append(start_time+position*pdres)
+                    values.append(value)
+
+    # Fill ts with missing values
+    ts = pd.Series(data=values,index=time_stamps)
+    if len(ts) == 0:
+        return ts
+    return ts.resample(pdres).ffill()
+
+def get_dayahead_prices(api_key: str, area_code: str, start: datetime, end: datetime, resolution: str):
+    fmt = '%Y%m%d%H00'  # Minutes must be 00, otherwise "HTTP 400 bad request" is returned.
+    url = f'https://web-api.tp.entsoe.eu/api?securityToken={api_key}&documentType=A44&in_Domain={area_code}' \
+          f'&out_Domain={area_code}&periodStart={start.strftime(fmt)}&periodEnd={end.strftime(fmt)}'
+
+    with urlopen(url) as response:  # Raises URLError
+        if response.status != 200:
+            raise Exception(f"{response.status=}")
+        xml_str = response.read().decode()
+        result = parse_timeseries(xml_str, resolution)
+
+    return result
+
+def update_day_ahead_prices(country_code, resolution):
     logging.debug("Getting day ahead prices for country {0} with resolution {1}".format(country_code, resolution))
     try:
         start = pd.Timestamp.today(tz='Europe/Berlin').replace(hour=0, minute=0, second=0, microsecond=0)
         end = start + timedelta(days=7)
 
-        ts     = client.query_day_ahead_prices(country_code, start=start, end=end, resolution=resolution)
+        ts     = get_dayahead_prices(ENTSOE_KEY, country_code, start, end, resolution)
         data   = ts.to_list()
         # Check if data has valid number of entries
         if resolution == '15min' and len(data) < 23*4:
@@ -61,9 +107,6 @@ def update_day_ahead_prices(client, country_code, resolution):
         next_date_ts = int(next_date.timestamp())
         logging.debug("Discovered new day ahead prices for {0}/{1}: {2}, next: {3}/{4}".format(str(start), str(first_date_ts), str(prices), str(next_date), str(next_date_ts)))
 
-    except entsoe.exceptions.NoMatchingDataError:
-        logging.debug('No matching data in entso-e database found for date {0}'.format(str(start)))
-        return None
     except:
         logging.error("Exception during entso-e query", exc_info=True)
         return None
@@ -75,9 +118,9 @@ def update_day_ahead_prices(client, country_code, resolution):
     od['next_date']  = next_date_ts
     return json.dumps(od, separators=(',', ':')), 200
 
-def update_day_ahead_prices_with_retry(client, country_code, resolution, retries=5):
+def update_day_ahead_prices_with_retry(country_code, resolution, retries=5):
     for retry in range(retries):
-        value = update_day_ahead_prices(client, country_code, resolution)
+        value = update_day_ahead_prices(country_code, resolution)
         if value != None:
             return value
         # Wait one minute more with every retry 
@@ -127,8 +170,8 @@ def is_update_necessary(dap, min_price_list_length):
 def update():
     for dap, country_code, resolution in daps():
         logging.debug("Check update for {0} {1}".format(country_code, resolution))
-        if is_update_necessary(dap_list[dap], 25 if resolution == '60min' else 25*4):
-            dap_list[dap] = update_day_ahead_prices_with_retry(entsoe_client, country_code, resolution)
+        if is_update_necessary(dap_list[dap], 25 if resolution == 'PT60M' else 25*4):
+            dap_list[dap] = update_day_ahead_prices_with_retry(country_code, resolution)
 
 # TODO: Rate limit per IP
 @day_ahead_prices_api.route('/v1/day_ahead_prices/<country>/<resolution>', methods=['GET'])
